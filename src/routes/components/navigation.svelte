7537c5e7d911b3860ffc3a6ba6bdf51c9cf04b7d
N3ttX <n3ttx0@gmail.com>
2021-09-25 23:37:06 +0200

    Introducing Foundation CSS
    Cards, come custom stylign

---
+++ src/routes/components/navigation.svelte
@@ -0,0 +1,18 @@
+<!-- Navigation -->
+<div class="top-bar" id="realEstateMenu">
+    <div class="top-bar-left">
+        <ul class="menu" data-responsive-menu="accordion">
+            <li class="menu-text">Zeptejte se studenta</li>
+            <li><a href="#">Úvod</a></li>
+            <li><a href="#">Položit dotaz</a></li>
+            <li><a href="#">Články</a></li>
+        </ul>
+    </div>
+    <div class="top-bar-right">
+        <ul class="menu">
+            <li><a href="#">Můj účet</a></li>
+            <li><a class="button">Login</a></li>
+        </ul>
+    </div>
+</div>
+<!-- /Navigation -->
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -19,19 +19,33 @@
 </script>
 
 <script>
+    import Navigation from "./components/navigation.svelte"
     export let post;
     console.log(post)
 </script>
 
-<h1>yo index</h1>
+<Navigation />
 
-{#each post.db as article}
-<h2>
-    {article.name}
-</h2>
+<div class="row small-up-1 medium-up-2 large-up-3">
+    {#each post.db as article}
+    <div class="card">
+        <div class="card-divider">
+            <b>{article.name}</b>
+        </div>
 
-<pre>
-    {article.text}
-</pre>
+        <div class="card-section">
+            {article.text}
+        </div>
+    </div>
+    {/each}
 
-{/each}
+    <div class="card">
+        <div class="card-divider">
+            <b>Examplke</b>
+        </div>
+
+        <div class="card-section">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt nemo incidunt vel soluta temporibus illo mollitia doloribus iure reprehenderit harum, amet ipsam, omnis nulla dolorem impedit voluptas nihil reiciendis distinctio!
+        </div>
+    </div>
+</div>
