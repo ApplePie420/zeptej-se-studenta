2616eb4e30cd0b034da322b620d2bc1bf85ff71f
N3ttX <n3ttx0@gmail.com>
2021-09-29 13:10:24 +0200

    cards description, translation, animation; mobile design fix

--- src/routes/components/cards-index.svelte
+++ src/routes/components/cards-index.svelte
@@ -3,59 +3,99 @@
 </script>
 
 <div class="grid-x">
-	<div class="cell small-3">
+	<div class="cell large-3">
 		<div class="custom-card radius">
-			<img src="../../static/cards-icons/home.svg" alt="home" class="card-icon" />
-			{$_('cards_categories.property')}
+			<div class="main-text">
+				<img src="../../static/cards-icons/home.svg" alt="home" class="card-icon" />
+				{$_('cards_categories.property')}
+			</div>
+			<div class="hover-text">
+				{$_('cards_description.property')}				
+			</div>
 		</div>
 	</div>
 
-	<div class="cell small-3">
+	<div class="cell large-3">
 		<div class="custom-card radius">
-			<img src="../../static/cards-icons/family.svg" alt="home" class="card-icon" />
-			{$_('cards_categories.family')}
+			<div class="main-text">
+				<img src="../../static/cards-icons/family.svg" alt="home" class="card-icon" />
+				{$_('cards_categories.family')}
+			</div>
+			<div class="hover-text">
+				{$_('cards_description.family')}				
+			</div>
 		</div>
 	</div>
 
-	<div class="cell small-3">
+	<div class="cell large-3">
 		<div class="custom-card radius">
-			<img src="../../static/cards-icons/law.svg" alt="home" class="card-icon" />
-			{$_('cards_categories.law')}
+			<div class="main-text">
+				<img src="../../static/cards-icons/law.svg" alt="home" class="card-icon" />
+				{$_('cards_categories.law')}
+			</div>
+			<div class="hover-text">
+				{$_('cards_description.law')}				
+			</div>
 		</div>
 	</div>
 
-	<div class="cell small-3">
+	<div class="cell large-3">
 		<div class="custom-card radius">
-			<img src="../../static/cards-icons/suitcase.svg" alt="home" class="card-icon" />
-			{$_('cards_categories.work')}
+			<div class="main-text">
+				<img src="../../static/cards-icons/suitcase.svg" alt="home" class="card-icon" />
+				{$_('cards_categories.work')}
+			</div>
+			<div class="hover-text">
+				{$_('cards_description.work')}				
+			</div>
 		</div>
 	</div>
 
-	<div class="cell small-3">
+	<div class="cell large-3">
 		<div class="custom-card radius">
-			<img src="../../static/cards-icons/company.svg" alt="home" class="card-icon" />
-			{$_('cards_categories.contracts')}
+			<div class="main-text">
+				<img src="../../static/cards-icons/company.svg" alt="home" class="card-icon" />
+				{$_('cards_categories.contracts')}
+			</div>
+			<div class="hover-text">
+				{$_('cards_description.contracts')}				
+			</div>
 		</div>
 	</div>
 
-	<div class="cell small-3">
+	<div class="cell large-3">
 		<div class="custom-card radius">
-			<img src="../../static/cards-icons/argument.svg" alt="home" class="card-icon" />
-			{$_('cards_categories.neighbourhood')}
+			<div class="main-text">
+				<img src="../../static/cards-icons/argument.svg" alt="home" class="card-icon" />
+				{$_('cards_categories.neighbourhood')}
+			</div>
+			<div class="hover-text">
+				{$_('cards_description.neighbourhood')}				
+			</div>
 		</div>
 	</div>
 
-	<div class="cell small-3">
+	<div class="cell large-3">
 		<div class="custom-card radius">
-			<img src="../../static/cards-icons/legal.svg" alt="home" class="card-icon" />
-			{$_('cards_categories.responsibility')}
+			<div class="main-text">
+				<img src="../../static/cards-icons/legal.svg" alt="home" class="card-icon" />
+				{$_('cards_categories.responsibility')}
+			</div>
+			<div class="hover-text">
+				{$_('cards_description.responsibility')}				
+			</div>
 		</div>
 	</div>
 
-	<div class="cell small-3">
+	<div class="cell large-3">
 		<div class="custom-card radius">
-			<img src="../../static/cards-icons/placeholder.svg" alt="home" class="card-icon" />
-			{$_('cards_categories.placeholder')}
+			<div class="main-text">
+				<img src="../../static/cards-icons/placeholder.svg" alt="home" class="card-icon" />
+				{$_('cards_categories.placeholder')}
+			</div>
+			<div class="hover-text">
+				{$_('cards_description.placeholder')}				
+			</div>
 		</div>
 	</div>
 </div>
