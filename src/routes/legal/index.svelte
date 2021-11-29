ea43ed13abe47805ba1a340d57dd16e91ea680b4
N3ttX <n3ttx0@gmail.com>
2021-11-29 22:50:58 +0100

    /legal landing page

---
+++ src/routes/legal/index.svelte
@@ -0,0 +1,46 @@
+<script>
+	import { _, setupI18n, isLocaleLoaded, locale } from '$lib/i18n.js';
+
+	import Navigation from '../../components/navigation.svelte';
+
+	$: if (!$isLocaleLoaded) {
+		setupI18n({ withLocale: 'cz' });
+	}
+</script>
+
+{#if $isLocaleLoaded}
+	<Navigation />
+
+
+	<div class="container">
+		<div class="grid-x text-center">
+            <div class="cell small-4">
+                    <a href="/legal/tos">
+                    <h1>
+                        Terms and Conditions
+                    </h1>
+                </a>
+                </div>
+
+            <div class="cell small-4">
+                    <a href="/legal/privacy_policy">
+                    <h1>
+                        Privacy Policy
+                    </h1>
+                </a>
+                </div>
+            
+            <div class="cell small-4">
+                    <a href="/legal/codex">
+                    <h1>
+                        Kodex
+                    </h1>
+                </a>
+                </div>
+		</div>
+		
+	</div>
+	
+{:else}
+	<p>Loading...</p>
+{/if}
